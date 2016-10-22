44a06893058136d53152785137b12f6ac3f83b1e
dribehance <dom4j.for.networking@gmail.com>
2016-10-22 15:58:04 +0800

    change google map api only en

--- src/js/services/googleMapServices.js
+++ src/js/services/googleMapServices.js
@@ -52,7 +52,7 @@ angular.module("Skillopedia").factory("googleMapServices", function($http, $q, c
 		geocoding: function(input) {
 			return $http({
 				// by dribehance <dribehance.kksdapp.com>
-				url: "https://maps.googleapis.com/maps/api/geocode/json",
+				url: "https://maps.googleapis.com/maps/api/geocode/json?language=en",
 				method: "GET",
 				params: angular.extend({}, {
 					key: config.key.google
